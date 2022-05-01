aabd9ab0441c73d5cf385887cf6537e7942cb73e
Malik8008 <tu60prbs1@code.edu.az>
2022-05-01 08:57:24 +0400

    Library task complete

---
+++ Kitabxana_Project/Library.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kitabxana_Project
+{
+    internal class Library
+    {
+         public List<Book> Books = new List<Book>();
+
+        public List<Book> FindAllBooksByName(string book)
+        {
+            return Books.FindAll(x => x.Name.Contains(book));
+        }
+
+        public void RemoveAllBooksByName(string bk)
+        {
+            Console.WriteLine(Books.RemoveAll(x=>x.Name.Contains(bk)));
+        }
+
+        public List<Book> SearchBooks(string val)
+        {
+            return Books.FindAll(book => book.Name.Contains(val) || book.AuthorName.Contains(val) || book.PageCount.ToString().Contains(val));
+        }
+
+        public List<Book> FindAllBooksByPageCountRange(int min,int max)
+        {
+            return Books.FindAll(book => book.PageCount >= min && book.PageCount <= max);
+        }
+
+        public void RemoveByCode(string val)
+        {
+            Book wantedBook = Books.Find(book => book.Code == val);
+            if (wantedBook != null)
+            {
+                Books.Remove(wantedBook);
+            }
+        }
+
+
+    }
+}
---
+++ Kitabxana_Project/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Kitabxana_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Ewref","Kasib Yazar",112);
+            Book book2 = new Book("Dilenci","Semi Ram",65);
+            Book book3 = new Book("Kasib Ata","Albert Indi",107);
+            Book book4 = new Book("Canguden","Viktor Huqo",178);
+           
+           
+
+            Library library = new Library();
+            library.Books.Add(book1);
+            library.Books.Add(book2);  
+            library.Books.Add(book3);
+            library.Books.Add(book4);
+
+            //foreach (var item in library.FindAllBooksByName("Dilenci"))
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+
+            //library.RemoveAllBooksByName("Canguden");
+            //foreach (var item in library.Books)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            //foreach (var item in library.SearchBooks("Kasib Ata"))
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            //foreach (var item in library.FindAllBooksByPageCountRange(108,180))
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            library.RemoveByCode("EW10");
+            foreach (var item in library.Books)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
